0146d9bb068673c69096e565d53271816bd313a5
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2023-12-13 09:09:09 +0300

    Добавьте файлы проекта.

---
+++ HomeworkWithDelegateAndException/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HomeworkWithDelegateAndException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions =
+            {
+                new ArgumentException("Не верный аргумент"),
+                new Exception("Что то сломалось"),
+                new EnterException("Не верный ввод"),
+                new ArgumentNullException ("Входной аргумет"),
+                new ArgumentOutOfRangeException ("Входной аргумент")
+            };
+            foreach (Exception ex in exceptions)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch (Exception ex2)
+                {
+                    ConsoleColor defCol = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n=========================");
+                    Console.WriteLine("Вы вызвали ошибку");
+                    Console.WriteLine(ex2.Message);
+                    Console.WriteLine("=========================\n");
+                    Console.ForegroundColor = defCol;
+                }
+                finally
+                {
+                    Console.WriteLine( "Finally, do something");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
